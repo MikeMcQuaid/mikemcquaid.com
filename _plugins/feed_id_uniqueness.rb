15661b0d2463df11dfd4ae590a69374e5d1e9a0e
Mike McQuaid <mike@mikemcquaid.com>
2026-01-20 14:38:27 +0000

    feed_id_uniqueness: tweak style.

--- _plugins/feed_id_uniqueness.rb
+++ _plugins/feed_id_uniqueness.rb
@@ -1,16 +1,16 @@
 module Jekyll
   Jekyll::Hooks.register :site, :post_write do |site|
     errors = site.pages.filter_map do |page|
-      next unless page.data["layout"] == "atom_feed"
+      next if page.data["layout"] != "atom_feed"
 
       path = page.destination(site.dest)
       next unless File.exist?(path)
 
       ids = File.read(path).scan(/<entry>.*?<id>(.*?)<\/id>/m).flatten
-      dupes = ids.tally.select { |_, count| count > 1 }.keys
-      next if dupes.empty?
+      duplicate_ids = ids.tally.select { |_, count| count > 1 }.keys
+      next if duplicate_ids.empty?
 
-      "#{page.path || page.url} (#{dupes.join(", ")})"
+      "#{page.path || page.url} (#{duplicate_ids.join(", ")})"
     end
 
     raise Jekyll::Errors::FatalException, "Duplicate feed entry ids: #{errors.join(", ")}" if errors.any?
